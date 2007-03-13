d6aea9a93f77a5acb4f874b60e62bf8754bcfb67
Nicolas Roffet <nicolas-kde@roffet.com>
2007-03-13 20:34:26 +0000

    APIDOX. Header documentation to explain how the laser ray attributes should be defined in the theme file. CCMAIL: johann.ollivierlapeyre@gmail.com
    svn path=/trunk/KDE/kdegames/kblackbox/; revision=642281

--- kbbscalablegraphicwidgetnonsvgitems.h
+++ kbbscalablegraphicwidgetnonsvgitems.h
@@ -41,23 +41,66 @@
 /**
  * @brief Get some attributes in the SVG file for items that are not SVG items
  *
- * Useful for laser ray.
+ * This class is useful for laser rays.
+ * Laser rays inherit QGraphicsPathItem and not KBBGraphicsItem (i.e. not QGraphicsSvgItem). But we want to be able to configure their attributes (color, width and style) to suit the graphic theme. All other items are saved as SVG items in a single theme file. To make it easy to manage themes, the laser ray attributes are also saved in the same SVG file. And as SVG is a XML format, <b>this class parses the XML of the SVG file</b>.
+ *
+ * Prerequisites: The SVG file must follow a strict structure.
+ * - Laser ray elements must be defined in a XML node that is a direct child of the root SVG node. It won't work for instance if there is a "layer" node between the XML node of the laser ray and the root node.
+ * - Just the attribute "style" of the laser ray node is used. The color is the value of "stroke:", the width is the value of "stroke-width:", and the style is the value of "stroke-dasharray:". All other values are ignored.
+ * - The id attribute of the node is important, but the node tag is not. ("g" or "path" or whatever is OK).
+ *
  * @see KBBGraphicsItemRay
  */
 class KBBScalableGraphicWidgetNonSvgItems
 {
 	public:
+		/**
+		 * @brief Constructor
+		 *
+		 * The constructor opens the SVGZ file and get the DOM root node.
+		 * @param &svgzFile Path to the SVGZ file
+		 */
 		KBBScalableGraphicWidgetNonSvgItems(const QString &svgzFile);
 		
 		
+		/**
+		 * @brief Get the color of the item
+		 *
+		 * @param &elementId Value of the attribute "id" of the XML node of the considered item.
+		 * @return The color
+		 */
 		QColor color(const QString &elementId);
+		
+		/**
+		 * @brief Get the pen style of the item
+		 *
+		 * @param &elementId Value of the attribute "id" of the XML node of the considered item.
+		 * @return Qt::DotLine except if the style value "stroke-dasharray" is "none". In that case: Qt::SolidLine
+		 */
 		Qt::PenStyle style(const QString &elementId);
+		
+		/**
+		 * @brief Get the width of the item
+		 *
+		 * @param &elementId Value of the attribute "id" of the XML node of the considered item.
+		 * @return The stroke width
+		 */
 		qreal width(const QString &elementId);
 
 
 	private:
+		/**
+		 * @brief Get the value of the style attribute
+		 *
+		 * @param &elementId Value of the attribute "id" of the XML node of the considered item.
+		 * @param &styleElement Element of the attribute "style" to consider
+		 * @return Value of the given element in the attribute "style" of the give XML node
+		 */
 		QString value(const QString &elementId, const QString &styleElement);
 		
+		/**
+		 * @brief Root element of the DOM tree of the XML theme file
+		 */
 		QDomElement m_root;
 };
 
